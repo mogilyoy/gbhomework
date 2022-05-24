654b40b091a36f91fee7a2b48f66c834c602bb61
Matvey <mogilyoy@ya.ru>
2022-05-24 22:20:47 +0700

    4 task fix

--- task4.py
+++ task4.py
@@ -5,11 +5,10 @@
 # для классов TownCar и WorkCar переопределите метод show_speed. При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
 # Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат. Вызовите методы и покажите результат.
 
+
 class Car():
-
-    speed = None
-
     def __init__(self):
+        self.speed = None
         self.color = None
         self.name = None
         self.is_police = False
@@ -17,23 +16,23 @@ class Car():
         self.dir_state = self.directions[1] # по умолчанию машина едет на север
 
     def go(self, speed):
-        Car.speed = speed
+        self.speed = speed
         print(f'Стартуем! Текущая скорость машины: {speed}')
 
     def stop(self):
-        Car.speed = 0
+        self.speed = 0
         print('Приехали! Машина стоит на месте')
 
     def turn(self, direction):
         if direction == 'right' or direction == 'r':
-            if Car.speed != 0:
+            if self.speed != 0:
                 self.dir_state = self.directions[self.directions.index(self.dir_state)+1]  
                 # если мы поворачиваем направо, то смещаемся на один элемент вправо
                 print(f'Поворот направо, теперь машина едет на {self.dir_state}')
             else:
                 print('Машина стоит на месте, не могу повернуть')
         elif direction == 'left' or direction == 'l':
-            if Car.speed != 0:
+            if self.speed != 0:
                 self.dir_state = self.directions[self.directions.index(self.dir_state)-1]  
                 # если мы поворачиваем налево, то смещаемся на один элемент влево
                 print(f'Поворот налево, теперь машина едет на {self.dir_state}')
@@ -44,7 +43,7 @@ class Car():
 
     def show_speed(self):
         print(f'Машина едет со скоростью {Car.speed} км/ч')
-
+    
 
 class TownCar(Car):
     def __init__(self):
@@ -54,9 +53,9 @@ class TownCar(Car):
         self.name = 'Town car'
     def show_speed(self):
         if Car.speed >60:
-            print(f'Вы едете {Car.speed} км/ч. Это слишком быстро!')
+            print(f'Вы едете {self.speed} км/ч. Это слишком быстро!')
         else:
-            print(f'Вы едете {Car.speed} км/ч. Так держать!')
+            print(f'Вы едете {self.speed} км/ч. Так держать!')
 
 
 class WorkCar(Car):
@@ -67,9 +66,9 @@ class WorkCar(Car):
         self.name = 'Work car'
     def show_speed(self):
         if Car.speed >40:
-            print(f'Вы едете {Car.speed} км/ч. Это слишком быстро!')
+            print(f'Вы едете {self.speed} км/ч. Это слишком быстро!')
         else:
-            print(f'Вы едете {Car.speed} км/ч. Так держать!')
+            print(f'Вы едете {self.speed} км/ч. Так держать!')
 
 
 class SportCar(Car):
@@ -80,9 +79,9 @@ class SportCar(Car):
         self.name = 'Sport car'
     def show_speed(self):
         if Car.speed < 60:
-            print(f'Вы едете {Car.speed} км/ч. Добавьте газу!')
+            print(f'Вы едете {self.speed} км/ч. Добавьте газу!')
         else:
-            print(f'Вы едете {Car.speed} км/ч. Вы точно приедете первым!')
+            print(f'Вы едете {self.speed} км/ч. Вы точно приедете первым!')
 
 
 class PoliceCar(Car):
@@ -92,17 +91,16 @@ class PoliceCar(Car):
         self.color = 'white'
         self.name = 'Police car'
     def show_speed(self):
-        if Car.speed < 60:
-            print(f'Вы едете {Car.speed} км/ч. Добавьте газу, преступник уходит!')
+        if self.speed < 60:
+            print(f'Вы едете {self.speed} км/ч. Добавьте газу, преступник уходит!')
         else:
-            print(f'Вы едете {Car.speed} км/ч. Виу-виу, поймать преступника!')
+            print(f'Вы едете {self.speed} км/ч. Виу-виу, поймать преступника!')
 
 a = PoliceCar()
 a.go(150)
 a.turn('r')
 a.turn('l')
 a.show_speed()
-a.stop()
 print(a.color)
 print(a.name)
 print(a.speed)
